a967ad3162ae999d80c9cb5c94d5510b41845224
Lavanya Garg <142018717+lavanyagarg112@users.noreply.github.com>
2023-12-18 22:58:56 +0800

    root changed to index

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :posts, only: [:new, :create, :index, :show]
-  root "posts#new"
+  root "posts#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
