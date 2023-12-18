ea348654495c2f5645346a5338edfaf469161915
Lavanya Garg <142018717+lavanyagarg112@users.noreply.github.com>
2023-12-18 22:41:15 +0800

    post controller and new added'

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,12 @@
+class PostsController < ApplicationController
+    before_action :authenticate_user!
+
+    def index
+        @posts = Post.all 
+    end
+
+    def new
+        @post = Post.new
+    end
+
+end
